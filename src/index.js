a52f3c7fa71cb22c9e76ff593a12e6e15cd6b5c6
mostafa <mostafa.darweech@gmail.com>
2022-01-16 14:07:05 +0200

    ..

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import postsRouter from './routes/posts.js'
 import bodyParser from 'body-parser';
-const port =process.env.port || 5000
+const port =process.env.PORT || 5000
 const app=express()
 app.use(cors())
 app.use(bodyParser.json({limit:'30mb',extended:true}))
